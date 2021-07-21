f1ea8d1bca764fc61df6a1cbf090fbcc2b9a8b59
nicolas-trindade <nicolastrindade@hotmail.com>
2021-07-20 23:31:52 -0300

    Criação da insersação no banco de dados, POST

--- src/main/java/one/digitalinnovation/personapi/SwaggerConfig.java
+++ src/main/java/one/digitalinnovation/personapi/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package one.digitalinnovation.personapi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -10,13 +8,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-@SpringBootApplication
-public class PersonapiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PersonapiApplication.class, args);
-	}
-
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
-
